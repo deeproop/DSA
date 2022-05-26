969775e0ac6c7d8b41259c10704c29686edb1507
Deep Roop <deepsinghroop@gmail.com>
2022-05-26 19:29:34 +0530

    adjacency matix

---
+++ Graph.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+
+    //declaring adjacency matrix
+    int adj[n+1][n+1];
+    int i,u,v;
+    //taking edges input
+    for(i=0; i<m; i++) {
+        cin>>u>>v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+
+    }
+
+    return 0;
+
+}
+
+//this can be used for small values
+//Space C - n*n    Time C - O(n^2)

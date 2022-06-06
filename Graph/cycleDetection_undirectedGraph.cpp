ed9c563bf32c2c3d1a47fd87b9f5a5d4ddca35ec
Deep Roop <deepsinghroop@gmail.com>
2022-06-06 09:43:23 +0530

    added comments

--- Graph/cycleDetection_undirectedGraph.cpp
+++ Graph/cycleDetection_undirectedGraph.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool check(int s, int V, vector<int> adj[], vector<int> vis) {
-    queue<pair<int,int>> q;
+bool check(int s, int V, vector<int> adj[], vector<int> vis) {  
+    queue<pair<int,int>> q; // pair of integers entering the queue to keep track of the parent of the child node
 
-    vis[s] = true;
-    q.push({s,-1});
+    vis[s] = true;  
+    q.push({s,-1}); // if it is the staring node then its parent is assumed to be -1
 
     while(!q.empty()) {
-        int node = q.front().first;
-        int par = q.front().second;
+        int node = q.front().first; // current node
+        int par = q.front().second; // parent 
         q.pop();
 
-        for(auto it : adj[node]) {
+        for(auto it : adj[node]) { //checking for the adjacent edges
             if(!vis[it]) {
-                vis[it] = true;
+                vis[it] = true;  
                 q.push({it, node});
             }
-            else if(par != it) {
+            else if(par != it) { // if the adjacent node is not equal to the parent then we have found the cycle
                 return false;
             }
         }
@@ -25,10 +25,11 @@ bool check(int s, int V, vector<int> adj[], vector<int> vis) {
 }
 
 bool isCycle(int V, vector<int> adj[]) {
-    vector<int> vis(V+1);
+    vector<int> vis(V+1); // making a visited vector
+    //traversing the nodes
     for(int i=1; i<=V; i++) {
         if(!vis[i]) {
-            if(check(i, V, adj, vis)) {
+            if(check(i, V, adj, vis)) { //if this node is giving a cycle or not
                 return true;
             }
         }
